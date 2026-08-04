c58a4381e408611f7efb15e73fdaf2c4067fd510
Seongho Bae <me@seonghobae.me>
2026-08-04 20:23:14 +0900

    feat(renderer): export structured diagnostic contracts

--- packages/plantuml-renderer/src/index.js
+++ packages/plantuml-renderer/src/index.js
@@ -1,4 +1,7 @@
-export { parsePlantUmlStandardReport } from './diagnostics.js';
 export { PlantUmlRendererError } from './errors.js';
 export { createPlantUmlRenderer } from './renderer.js';
 export { plantUmlRendererLimits } from './limits.js';
+export {
+  parsePlantUmlStandardReport,
+  sanitizePlantUmlDiagnostics,
+} from './standard-report.js';
