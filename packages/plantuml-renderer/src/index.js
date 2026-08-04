cacaf2608858bed922d20de1fe0bbc2335a09d87
Seongho Bae <me@seonghobae.me>
2026-08-04 20:05:52 +0900

    feat(renderer): export standard-report parser

--- packages/plantuml-renderer/src/index.js
+++ packages/plantuml-renderer/src/index.js
@@ -1,3 +1,4 @@
+export { parsePlantUmlStandardReport } from './diagnostics.js';
 export { PlantUmlRendererError } from './errors.js';
 export { createPlantUmlRenderer } from './renderer.js';
 export { plantUmlRendererLimits } from './limits.js';
