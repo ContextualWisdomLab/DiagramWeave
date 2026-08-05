27d79e86a704fa1a24a48b1b30aec31f559fce7b
Seongho Bae <me@seonghobae.me>
2026-08-05 11:36:00 +0900

    feat(language-server): make document symbols part of the public session

--- packages/language-server/src/index.js
+++ packages/language-server/src/index.js
@@ -1,3 +1,5 @@
 export { LanguageServerError } from './errors.js';
 export { languageServerLimits } from './limits.js';
-export { createLanguageServerSession } from './session.js';
+export {
+  createDocumentSymbolLanguageServerSession as createLanguageServerSession,
+} from './symbol-session.js';
